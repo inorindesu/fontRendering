434aeb567bf5efdd8cc9539de0b818c9a6e0e2f7
Inori Sakura <inorindesu@gmail.com>
2013-02-26 23:21:31 +0800

    Initial setup of the project

---
+++ ft2_char.c
@@ -0,0 +1,24 @@
+/* 
+ * Render single character from selected font, then output the result into
+ * stdout.
+ *
+ * Usage:
+ * ft2_char character fontPath > output.png
+ *
+ * Example:
+ * ft2_char M /usr/share/fonts/gnu-free/FreeSans.ttf
+ * 
+ * Copyright (C) 2013  Inori Sakura <inorindesu@gmail.com>
+ * 
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See the COPYING file for more details.
+ */
+
+
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+int main(int argc, char** argv)
+{
+}
